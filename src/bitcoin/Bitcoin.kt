855ee5158374dac39e7a9309940e7302c78914e9
Karolis Simanavičius <56815036+knuckl35@users.noreply.github.com>
2020-11-03 20:26:35 +0200

    generate users, transactions

--- src/bitcoin/Bitcoin.kt
+++ src/bitcoin/Bitcoin.kt
@@ -3,28 +3,38 @@ package bitcoin
 import hash.HashAlgorithm
 import kotlin.streams.asSequence
 
-
 private val hashAlgorithm = HashAlgorithm()
 val userList: ArrayList<User> = arrayListOf()
 val transactionList: ArrayList<Transaction> = arrayListOf()
+val transactionListIds: ArrayList<String> = arrayListOf()
 
-fun main(args: Array<String>) {
-    for(i in 0..10){
+val blockchain: ArrayList<Block> = arrayListOf()
+
+fun main() {
+
+    for(i in 0..100){
         userList.add(generateUser(i))
     }
-    for(i in 0..100){
+    for(i in 0..9) println(userList[i].balance)
+    println()
+
+    for(i in 0..1000){
         transactionList.add(generateTransaction(userList))
+        transactionListIds.add(transactionList[i].transactionId)
     }
+
+
+
+    println(merkleTree(transactionListIds))
 }
 
 
 private fun generateUser(index: Int): User{
     val name = "user$index"
     val publicKey = hashAlgorithm.hashFunction(generateRandomString())
-    val balance = (100..1000000).random()
+    val balance = (100..1000000).random().toDouble()
     return User(name, publicKey, balance)
 }
-
 
 private fun generateRandomString(): String{
     val source = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -34,16 +44,27 @@ private fun generateRandomString(): String{
             .joinToString("")
 }
 
-
 private fun generateTransaction(users: ArrayList<User>): Transaction{
-    val user = users[(0..10).random()]
-    val senderKey = user.publicKey
-    val receiverKey = users[(0..10).random()].publicKey
-    var amount = (100..1000000).random()
-
-    while(amount > user.balance) amount = (100..1000000).random()
+    val firstUserIndex = (0..100).random()
+    val secondUserIndex = (0..100).random()
+    val senderKey = users[firstUserIndex].publicKey
+    val receiverKey = users[secondUserIndex].publicKey
+    val amount = users[firstUserIndex].balance * 0.01
 
     val txId = hashAlgorithm.hashFunction(senderKey + receiverKey + amount.toString())
 
+    userList[firstUserIndex].pay(amount)
+    userList[secondUserIndex].receive(amount)
+
     return Transaction(txId, senderKey, receiverKey, amount)
 }
+
+private fun merkleTree(transactionsId: ArrayList<String>): String{
+    val tempTxList: ArrayList<String> = arrayListOf()
+    for(i in 0 until transactionsId.size - 1 step 2){
+        tempTxList.add(hashAlgorithm.hashFunction(transactionsId[i]+ transactionsId[i + 1]))
+    }
+    return if(tempTxList.size == 1) tempTxList[0]
+    else merkleTree(tempTxList)
+}
+
--- src/bitcoin/MainClasses.kt
+++ src/bitcoin/MainClasses.kt
@@ -2,18 +2,42 @@ package bitcoin
 
 import java.sql.Timestamp
 
-data class User(val name: String, val publicKey: String, val balance: Int)
+data class User(val name: String, val publicKey: String, var balance: Double){
+    fun isBalanceValid(amount: Double): Boolean{
+        return balance >= amount
+    }
 
-data class Transaction(val transactionId: String, val senderKey: String, val receiverKey: String, val sum: Int)
+    fun receive(amount: Double){
+        balance += amount
+    }
+
+    fun pay(amount: Double){
+        balance -= amount
+    }
+}
+
+data class Transaction(val transactionId: String, val senderKey: String, val receiverKey: String, val sum: Double){
+    fun validateTransaction(users: ArrayList<User>): Boolean{
+        val userPosition: Int? = users.indices.find {
+            users[it].publicKey == senderKey
+        }
+        return users[userPosition!!].balance >= sum
+    }
+}
 
 data class Block(
-        val previousBlock: String,
-        val currentBlock: String,
+        val previousBlock: String = "0",
+        var currentBlock: String = "",
         val timeStamp: Timestamp,
-        val version: String,
-        val merkelRoot: String,
-        val nonce: Int,
-        val difficultyTarget: String)
+        val version: String = "0.1",
+        val merkelRoot: String = "",
+        var nonce: Int = 0,
+        val difficultyTarget: String = "",
+        val transactions: ArrayList<Transaction> = arrayListOf()){
+    fun mine(){
+
+    }
+}
 
 
 
