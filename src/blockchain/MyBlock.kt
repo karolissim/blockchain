cb298c15b956f0f8a895e5de2e6bede69a5f045f
Karolis Simanavičius <56815036+knuckl35@users.noreply.github.com>
2020-11-12 22:45:57 +0200

    almost finished multi

--- src/blockchain/MyBlock.kt
+++ src/blockchain/MyBlock.kt
@@ -11,61 +11,77 @@ class MyBlock {
 
     companion object {
         private val blockchain: ArrayList<Block> = arrayListOf()
-        val UTXOs: MutableMap<String, TransactionOutput> = mutableMapOf()
+        var UTXOs: MutableMap<String, TransactionOutput> = mutableMapOf()
         private val userList: ArrayList<User> = arrayListOf()
         private val transactionPool: ArrayList<Transaction?> = arrayListOf()
         private const val txDifficulty = 2
         var previousBlockHash = "0"
         private var tempTransactionPool: ArrayList<MutableMap<Int, Transaction?>> = arrayListOf()
         var tempUTXOList: ArrayList<MutableMap<String, TransactionOutput>> = arrayListOf()
+        var isCalled: Boolean = false
+
+        var firstMiner: Job? = null
+        var secondMiner: Job? = null
+        var thirdMiner: Job? = null
+        var fourthMiner: Job? = null
 
         @JvmStatic
         fun main(args: Array<String>) {
             blockchain.add(generateGenesisBlock())
 
-            for(i in (0..99)) {
+            for (i in (0..99)) {
                 transactionPool.add(userList[(0..9).random()].sendMoney(userList[(0..9).random()].publicKey, userList[(0..9).random()].getBalance() * 0.05))
             }
 
-            for(i in (0..5)) {
-                for (j in (0..5)) {
-                    tempTransactionPool.add(generateTransactionMap())
-                    tempUTXOList.add(UTXOs)
-                }
+            for (j in (0..5)) {
+                tempTransactionPool.add(generateTransactionMap())
+                tempUTXOList.add(UTXOs)
             }
 
-            //coroutines
-            val job1 = GlobalScope.launch {
-                val newBlock0 = Block(timeStamp = Timestamp(System.currentTimeMillis()))
-                tempTransactionPool[0].values.forEach {
-                    newBlock0.addTransaction(it, 0)
-                }
-                if(newBlock0.mine(txDifficulty, 2000)) {
-                    newBlock0.previousBlock = previousBlockHash
-                    blockchain.add(newBlock0)
-                    previousBlockHash = newBlock0.getHash()
-                } else {
-                    println("Unable to mine block")
-                }
-            }
+            GlobalScope.launch {
 
-            val job2 = GlobalScope.launch {
-                val newBlock1 = Block(timeStamp = Timestamp(System.currentTimeMillis()))
-                tempTransactionPool[1].values.forEach {
-                    newBlock1.addTransaction(it, 1)
-                }
-                if(newBlock1.mine(txDifficulty, 2000)) {
-                    newBlock1.previousBlock = previousBlockHash
-                    blockchain.add(newBlock1)
-                    previousBlockHash = newBlock1.getHash()
-                }else {
-                    println("Unable to mine block")
+                while(transactionPool.size >= 10) {
+                    println("$isCalled ${transactionPool.size}")
+                    if (isActive && !isCalled) {
+                        firstMiner = launch(Dispatchers.IO) {
+                            mine(0, object : JobCallback {
+                                override suspend fun onFinish(status: Boolean, index: Int?, block: Block?) {
+                                    if (status) {
+                                        if(!isCalled) {
+                                            isCalled = true
+                                            blockchain.add(block!!)
+                                            previousBlockHash = block.getHash()
+                                            println("finished: $index")
+                                            cancelJob(index!!)
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        secondMiner = launch(Dispatchers.IO) {
+                            delay(500L)
+                            mine(1, object : JobCallback {
+                                override suspend fun onFinish(status: Boolean, index: Int?, block: Block?) {
+                                    if (status) {
+                                        if(!isCalled) {
+                                            isCalled = true
+                                            blockchain.add(block!!)
+                                            previousBlockHash = block.getHash()
+                                            println("finished: $index")
+                                            cancelJob(index!!)
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                        delay(4000L)
+                    } else isCalled = false
                 }
             }
 
             runBlocking {
-                job1.join()
-                job2.join()
+                delay(50000L)
                 printData()
             }
         }
@@ -103,19 +119,6 @@ class MyBlock {
             return transaction
         }
 
-        private fun generateBlock(): Block {
-            val block = Block(timeStamp = Timestamp(System.currentTimeMillis()))
-
-            for (i in (0..99)) {
-                block.addTransaction(userList[(0..9).random()].sendMoney(userList[(0..9).random()].publicKey, userList[(0..9).random()].getBalance() * 0.05))
-            }
-
-            block.mine(txDifficulty, 500)
-            previousBlockHash = block.getHash()
-
-            return block
-        }
-        
         private fun printData() {
             for (block in blockchain) {
                 println("currentBlock: ${block.getHash()} prevBlock: ${block.previousBlock}")
@@ -127,12 +130,65 @@ class MyBlock {
 
         private fun generateTransactionMap(): MutableMap<Int, Transaction?> {
             val txMap: MutableMap<Int, Transaction?> = mutableMapOf()
-            for(i in (0..9)) {
+            for (i in (0..9)) {
                 val random = (0 until transactionPool.size).random()
                 txMap[random] = transactionPool[random]
             }
             return txMap
         }
 
+        private suspend fun createJob(index: Int, jobCallback: JobCallback): Job {
+
+            return GlobalScope.launch {
+                val block = Block(timeStamp = Timestamp(System.currentTimeMillis()))
+                tempTransactionPool[index].values.forEach {
+                    block.addTransaction(it, index)
+                }
+                if (block.mine(txDifficulty, 2000)) {
+                    block.previousBlock = previousBlockHash
+                    blockchain.add(block)
+                    previousBlockHash = block.getHash()
+                    jobCallback.onFinish(true, index, block)
+                } else {
+                    jobCallback.onFinish(false, null, null)
+                    println("Unable to mine block")
+                }
+            }
+        }
+
+        private suspend fun mine(index: Int, jobCallback: JobCallback) {
+            val block = Block(timeStamp = Timestamp(System.currentTimeMillis()))
+            tempTransactionPool[index].values.forEach {
+                block.addTransaction(it, index)
+            }
+
+            if (block.mine(txDifficulty, 7000)) {
+                block.previousBlock = previousBlockHash
+                jobCallback.onFinish(true, index, block)
+            } else {
+                jobCallback.onFinish(false, null, null)
+                println("Unable to mine block")
+            }
+        }
+
+        private fun cancelJob(index: Int) {
+            firstMiner!!.cancel()
+            secondMiner!!.cancel()
+            println("CALLED")
+            UTXOs = tempUTXOList[index]
+            for((_, value) in tempTransactionPool[index]){
+                transactionPool.remove(value)
+            }
+            for (j in (0..5)) {
+                tempTransactionPool.add(generateTransactionMap())
+                tempUTXOList.clear()
+                tempUTXOList.add(UTXOs)
+            }
+            println("CANCELED")
+        }
+    }
+
+    interface JobCallback {
+        suspend fun onFinish(status: Boolean, index: Int?, block: Block?)
     }
 }
